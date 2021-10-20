4cd524e5205652f51ce1c062dc42597321309af3
Brendow Catarina Dellarmelin <brendowcata1@gmail.com>
2021-10-19 21:48:09 -0300

    CardProduto and CardEntradaSaida

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import home
+from .views import home, home_entrada_saida, home_produtos
 
 
 urlpatterns = [
-    path('', home),
+    path('', home, name="home"),
+    path('produtos/', home_produtos, name="produtos"),
+    path('entrada-saida/', home_entrada_saida, name="entrada-saida"),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from rest_framework.views import APIView
+from produtos import services
+from .services import ProdutoService
+
+_SERVICE = ProdutoService()
 
 def home(request):
     return render(request, 'home.html')
+
+
+def home_produtos(request):
+    produtos = _SERVICE.buscar_todos_produtos()
+    paginator = Paginator(produtos, 3)
+    page = request.GET.get('p')
+    produtos = paginator.get_page(page)
+    return render(request=request, template_name='home_produtos.html', context={'produtos': produtos})
+
+
+def home_entrada_saida(request):
+    return render(request, 'home_entrada_saida.html')
+    
